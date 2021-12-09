edf9413872d95dbe76ae858483a2867484734604
zoubenjia <53398369+zoubenjia@users.noreply.github.com>
2021-12-09 16:06:58 +1100

    add download latest vim zip

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,12 @@
+$vim="~\Download\vim"
+$vimURL = "https://github.com/vim/vim-win32-installer/releases/download/v8.2.3761/gvim_8.2.3761_x64.zip"
+$vimzip = $vim+"\zip\latest.zip"
+if (-not (test-path $vim)) 
+{
+    ni -it dir $vim+"\zip"
+    iwr $vimURL -OutFile $vimzip
+    Expend-Archive $vimzip -force 
+}
 Set-PSReadLineOption -EditMode vi -BellStyle None
 New-Alias -Name vi -Value '~\Downloads\vim\vim.exe'
 New-Alias -Name vim -Value '~\Downloads\vim\vim.exe'
