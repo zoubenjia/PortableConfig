e075bc7c599741a3beaf8b9a384f9e7a71485197
zoubenjia <53398369+zoubenjia@users.noreply.github.com>
2021-12-09 15:14:59 +1100

    Add files via upload

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+Set-PSReadLineOption -EditMode vi -BellStyle None
+New-Alias -Name vi -Value 'E:\Profiles\bzou\Downloads\vim\vim.exe'
+New-Alias -Name vim -Value 'E:\Profiles\bzou\Downloads\vim\vim.exe'
