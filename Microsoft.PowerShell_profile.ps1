5335afab55818a28c1373f7cc2e138b59f53d511
zoubenjia <53398369+zoubenjia@users.noreply.github.com>
2021-12-09 17:31:10 +1100

    tested on local pc

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,16 @@
 $vim="~\Download\vim"
 $vimURL = "https://github.com/vim/vim-win32-installer/releases/download/v8.2.3761/gvim_8.2.3761_x64.zip"
-$vimzip = $vim+"\zip\latest.zip"
+$vimzip = $env:TEMP+"\vim.zip"
+$vimbin = $HOME+"\Download\vim\vim\vim82\vim.exe"
 if (-not (test-path $vim)) 
 {
-    ni -it dir $vim+"\zip"
-    iwr $vimURL -OutFile $vimzip
-    Expend-Archive $vimzip -force 
+    #New-Item -it dir $vim"\zip"
+    Invoke-WebRequest $vimURL -OutFile $vimzip
+    Set-Location "~\Download"
+    Expand-Archive $vimzip -force 
 }
+remove-Alias -Name vi 
+remove-Alias -Name vim
+New-Alias -Name vi -Value $vimbin
+New-Alias -Name vim -Value $vimbin
 Set-PSReadLineOption -EditMode vi -BellStyle None
-New-Alias -Name vi -Value '~\Downloads\vim\vim.exe'
-New-Alias -Name vim -Value '~\Downloads\vim\vim.exe'
