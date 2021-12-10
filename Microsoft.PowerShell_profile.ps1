d4f5ff77ea77f3df4d83045538227db0d5f3fe3c
zoubenjia <53398369+zoubenjia@users.noreply.github.com>
2021-12-10 16:12:51 +1100

    add arch var

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,41 @@
-$vim="~\Download\vim"
-$vimURL = "https://github.com/vim/vim-win32-installer/releases/download/v8.2.3761/gvim_8.2.3761_x64.zip"
-$vimzip = $env:TEMP+"\vim.zip"
-$vimbin = $HOME+"\Download\vim\vim\vim82\vim.exe"
+$vim="$HOME\Downloads\vim"
 if (-not (test-path $vim)) 
 {
-    #New-Item -it dir $vim"\zip"
-    Invoke-WebRequest $vimURL -OutFile $vimzip
-    Set-Location "~\Download"
-    Expand-Archive $vimzip -force 
+    try{
+        $vimzip = "$env:TEMP\vim.zip"
+        if (-not (test-path $vimzip))
+        {   
+            $vimlatest = Invoke-RestMethod https://api.github.com/repos/vim/vim-win32-installer/releases/latest
+            switch -Regex ($env:PROCESSOR_ARCHITECTURE) {
+                '64' { $osarc = 'x64'; Break }
+                '32' { $osarc = 'x32'; Break }
+                '86' { $osarc = 'x86'; Break }
+                Default {$osarc = 'x64'}
+            }
+            Write-Host "OS arch is: "$osarc
+            $dlpattern = "((https://github.*?_$osarc.zip))"
+            $URLmatches = Select-String -Pattern $dlpattern -InputObject $vimlatest.body
+            $vimURL = $URLmatches.Matches.Value
+            if ($vimURL)
+            {
+                write-host "Find latest vim zip download URL: $vimURL"
+            }
+            else {
+                write-host "empty url!"
+            }
+            Invoke-WebRequest $vimURL -OutFile $vimzip
+        }
+        Set-Location "$HOME\Downloads"
+        write-host "extracting vim zip"
+        Expand-Archive $vimzip 
+        $vimbin = "$HOME\Downloads\vim\vim\vim82\vim.exe"
+        set-Alias -Name vi -Value $vimbin
+        set-Alias -Name vim -Value $vimbin
+        }
+
+    catch{
+        write-host "Error!"
+        Write-Host $_
+    }
 }
-remove-Alias -Name vi 
-remove-Alias -Name vim
-New-Alias -Name vi -Value $vimbin
-New-Alias -Name vim -Value $vimbin
 Set-PSReadLineOption -EditMode vi -BellStyle None
